aa4492b34c88684a010c33d3a626381c1ded2d59
Simone D'Amilo <simodamilo@gmail.com>
2025-07-17 18:13:25 +0200

    started workout page

--- src/pages/workouts/Workouts.tsx
+++ src/pages/workouts/Workouts.tsx
@@ -1,3 +1,23 @@
+import { FloatButton, Radio } from "antd";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 export const Workouts = () => {
-    return <div>Workouts</div>;
+    const { t } = useTranslation();
+    const [page, setPage] = useState<string>("current");
+
+    return (
+        <div className="w-full h-screen md:w-3xl flex flex-col gap-2 p-4 pb-16">
+            <Radio.Group value={page} onChange={(e) => setPage(e.target.value)}>
+                <Radio.Button value="current" className="w-1/2">
+                    {t("workouts.current_tab")}
+                </Radio.Button>
+                <Radio.Button value="history" className="w-1/2">
+                    {t("workouts.history_tab")}
+                </Radio.Button>
+            </Radio.Group>
+
+            <FloatButton onClick={() => console.log("onClick")} style={{ bottom: 80 }} />
+        </div>
+    );
 };
