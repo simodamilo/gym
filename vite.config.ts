1723a52180eb7926523ad792a63dea505a5520fe
Simone <simodamilo@gmail.com>
2025-08-06 18:31:31 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "/gym/",
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                navigateFallback: '/gym/index.html',
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+            },
+            manifest: {
+                name: 'My React PWA',
+                short_name: 'ReactPWA',
+                description: 'My awesome React PWA built with Vite',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+                start_url: '.',
+                display: 'standalone',
+                background_color: '#ffffff',
+            },
+        }),
+    ],
     server: {
         fs: {
             strict: true,
